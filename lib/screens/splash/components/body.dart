1d61fac650248dc3ba5b9fbeece59dc19e84db59
Ravishan Ranaweera <54973880+Ravishansachin@users.noreply.github.com>
2021-01-17 00:38:46 +0530

    Create body.dart

---
+++ lib/screens/splash/components/body.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:medicina/components/default_button.dart';
+import 'package:medicina/constants.dart';
+import 'package:medicina/screens/sign_in/sign_in_screen.dart';
+import 'package:medicina/screens/splash/components/splash_content.dart';
+import 'package:medicina/size_config.dart';
+
+//This is the best practice
+import '../components/splash_content.dart';
+import '../../../components/default_button.dart';
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int currentPage = 0;
+  List<Map<String, String>> splashData = [
+    {
+      "image": "assests/images/welcome1.png",
+      "text": "Our Medication | Your Satisfaction",
+    },
+    {
+      "image": "assests/images/welcome2.png",
+      "text": "Give to us your prescription",
+    },
+    {
+      "image": "assests/images/welcome3.png",
+      "text": "We deliver to your doorstep",
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                onPageChanged: (value) {
+                  setState(() {
+                    currentPage = value;
+                  });
+                },
+                itemCount: splashData.length,
+                itemBuilder: (context, index) => SplashContent(
+                  image: splashData[index]["image"],
+                  text: splashData[index]["text"],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(20)),
+                child: Column(
+                  children: <Widget>[
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        splashData.length,
+                        (index) => buildDot(index: index),
+                      ),
+                    ),
+                    Spacer(flex: 5),
+                    DefaultButton(
+                      text: "Continue",
+                      press: () {
+                        Navigator.pushNamed(context, SignInScreen.routeName);
+                      },
+                    ),
+                    Spacer(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AnimatedContainer buildDot({int index}) {
+    return AnimatedContainer(
+      duration: kAnimationDuration,
+      margin: EdgeInsets.only(
+        right: 5,
+      ),
+      height: 6,
+      width: currentPage == index ? 20 : 6,
+      decoration: BoxDecoration(
+        color: currentPage == index ? kPrimaryColor : Color(0xFFD8D8D8),
+        borderRadius: BorderRadius.circular(3),
+      ),
+    );
+  }
+}
