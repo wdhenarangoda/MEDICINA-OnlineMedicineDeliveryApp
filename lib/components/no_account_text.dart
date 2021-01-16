178d2f1cb90a92a04e55c17169d2e1aaeb65d07b
Thanura gayanaka <57209475+ThanuraGayanaka@users.noreply.github.com>
2021-01-17 00:36:13 +0530

    Create no_account_text.dart

---
+++ lib/components/no_account_text.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:medicina/screens/forgot_password/forgot_password_Screen.dart';
+
+import '../constants.dart';
+import '../size_config.dart';
+
+class NoAccountText extends StatelessWidget {
+  const NoAccountText({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Don't have an account? ",
+          style: TextStyle(fontSize: getProportionateScreenWidth(13)),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Text(
+            "Sign Up",
+            style: TextStyle(
+                fontSize: getProportionateScreenWidth(13),
+                color: kPrimaryColor),
+          ),
+        ),
+      ],
+    );
+  }
+}
